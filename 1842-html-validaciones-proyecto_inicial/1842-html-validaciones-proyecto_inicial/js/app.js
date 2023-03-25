fd0e757033b47a9a18196f6d3ad7262ea714cf3a
Bartolo Del Rosario Marmolejos <103548134+JD100423@users.noreply.github.com>
2023-03-25 17:25:09 -0400

    Update

--- 1842-html-validaciones-proyecto_inicial/1842-html-validaciones-proyecto_inicial/js/app.js
+++
@@ -1,9 +0,0 @@
-import { validar } from "./validaciones.js";
-
-const inputs = document.querySelectorAll("input")
-
-inputs.forEach(input => {
-    input.addEventListener("blur", (input => {
-        validar(input.target)
-    }))
-})
